e38e26a9fc3e80f3252d3dff4b3fcff27fa21674
Jack Watters <johnwatt@usc.edu>
2022-10-08 23:34:39 -0700

    html + css

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # weather-app
 
-- Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
-- Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
 - Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).
+
 - Display the information on your webpage!
-- Add any styling you like!
+- Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
+
 - Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
+/* eslint-disable comma-dangle */
 import './styles/style.css';
+
+const API_KEY = '8eefd61a1a12a4ebaa7a93aed80f367c';
+
+// TODO process the data once I know what I need
+function processData(data) {
+  console.log(data);
+  // precipitstion
+}
+
+async function getData(lat, lon) {
+  try {
+    // fetch locations weather data
+    const weatherResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
+    );
+    const weatherData = await weatherResponse.json();
+    return processData(weatherData);
+  } catch {
+    return console.error();
+  }
+}
+
+async function getCoordinates(location) {
+  try {
+    // fetch location data
+    const locationResponse = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${API_KEY}`
+    );
+    const locationData = await locationResponse.json();
+    // retrieve latitude and longitude from data
+    const { lat } = locationData[0];
+    const { lon } = locationData[0];
+    // get data using lat + lon
+    return getData(lat, lon);
+  } catch {
+    return console.error();
+  }
+}
+
+getCoordinates('mill valley');
